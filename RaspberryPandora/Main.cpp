928023afbf7dd1493fad2d3b809a9e2157d5e785
brian <brian@192.168.1.4>
2013-01-25 17:57:40 -0500

    initial commit of c++ project and some base files

---
+++ RaspberryPandora/Main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   Main.cpp
+ * Author: brian
+ *
+ * Created on January 24, 2013, 9:37 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include "PandoraAPI.h"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    string username;
+    string password;
+    cout << "Enter your username: ";
+    cin >> username;
+	cout << "Enter your password: ";
+    cin >> password;
+    
+    PandoraAPI api (username, password);
+    api.logOut();
+    
+    return 0;
+}
+
